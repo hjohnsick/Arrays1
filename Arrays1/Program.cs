cff9bd3e40358ab3c6fdcc582ddba0b44cf681e6
heather <hjohnsick@yahoo.com>
2020-04-13 15:21:51 -0400

    Fixed bug in loop so it iterates through array

---
+++ Arrays1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int index = -1;
+            string userContinue = "y";
+            bool found = false;
+            int[] numbers = { 2, 8, 0, 24, 51 };
+
+
+            while(userContinue == "y")
+            {
+                int usersNumber = GetUserInput("Enter a number: ");
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] == usersNumber)
+                    {
+                        index = i;
+                        found = true;
+                        break;
+                                       
+                    }
+                                    
+                }
+
+                if (found == true)
+                {
+                    Console.WriteLine(usersNumber + " is at position " + index);
+                }
+                else
+                {
+                    Console.WriteLine(usersNumber + " could not be found");
+                }
+
+                userContinue = Continue("Would you like to try another number? (y/n)").ToLower();
+
+                if (userContinue == "n")
+                {
+                    Console.WriteLine("Good bye!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            int input = int.Parse(Console.ReadLine());
+            return input;
+        }
+
+        public static string Continue(string message)
+        {
+            Console.WriteLine(message);
+            string userContinue = Console.ReadLine();
+            return userContinue;
+        }
+    }
+}
